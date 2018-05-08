1acaa4dfd633df87791ad094d3a62ab70680e4a6
Junhao Li <streaver91@gmail.com>
2018-05-08 16:35:31 -0400

    .

---
+++ src/broadcast_test.cc
@@ -0,0 +1,23 @@
+#include "broadcast.h"
+
+#include <gtest/gtest.h>
+#include <string>
+#include <unordered_map>
+
+TEST(BroadcastTest, Integer) {
+  int a = 0;
+  if (fgpl::internal::MpiUtil::is_master()) {
+    a = 3;
+  }
+  fgpl::broadcast(a);
+  EXPECT_EQ(a, 3);
+}
+
+TEST(BroadcastTest, UnorderedMap) {
+  std::unordered_map<std::string, int> a;
+  if (fgpl::internal::MpiUtil::is_master()) {
+    a["three"] = 3;
+  }
+  fgpl::broadcast(a);
+  EXPECT_EQ(a["three"], 3);
+}
