ab2f486e03d025607f26e67a95b3834da6b59cd2
Junhao Li <streaver91@gmail.com>
2018-05-17 16:59:51 -0400

    .

--- src/internal/hash/concurrent_hash_map.h
+++ src/internal/hash/concurrent_hash_map.h
@@ -91,10 +91,7 @@ void ConcurrentHashMap<K, V, H>::async_set(
 template <class K, class V, class H>
 V ConcurrentHashMap<K, V, H>::get(
     const K& key, const size_t hash_value, const V& default_value) const {
-  const size_t segment_id = hash_value % n_segments;
-  auto& lock = segment_locks[segment_id];
-  HashMap<K, V, H>* segment_ptr = &segments[segment_id];
-  V res = segment_ptr->get(key, hash_value, default_value);
+  V res = segments[segment_id].get(key, hash_value, default_value);
   return res;
 }
 
